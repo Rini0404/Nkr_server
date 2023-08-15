dfd013b0cd65e2310fd250c61caa8fb163b87f50
Rini <Reneortega2468@gmail.com>
2023-08-15 19:31:30 -0400

    Chinks being recieved!

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,40 @@
-// src/server.ts
-
-import express from 'express';
-import http from 'http';
-import { initializeWebSocket } from './socket';
+const { Server } = require("socket.io");
+import express from "express";
+import http from "http";
 
 const app = express();
 const PORT = 3000;
 
 const server = http.createServer(app);
 
-// Initialize WebSocket with the HTTP server
-initializeWebSocket(server);
+type Message = {
+    message: string;
+};
 
-app.get('/', (req, res) => {
-    res.send('Hello from the TypeScript server!');
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    },
+});
+
+
+
+io.on("connection", (socket: any) => {
+    console.log("a user connected");
+    socket.emit("message", "Hello from server");
+    socket.on('audio_chunk', (audioData: any) => {
+        // Here, handle the audio data. For now, just printing it:
+        console.log('Received audio chunk')
+    })
+});
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from the TypeScript server!");
 });
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
